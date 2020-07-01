c07a8dde0d5822cce14d7d8777f4e5200e13ab9d
koyeary <katyeary@gmail.com>
2020-07-01 16:25:10 -0700

    debugged server issue, all router functions successful

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,42 +6,54 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", (req, res) => {
-  burger.all = (data) => {
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
     const hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
-  }
-});
+  });
 
-router.post("/api/burgers", (req, res) => {
-  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], (result) => {
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", (req, res) => {
+router.put("/api/burgers/:id", function(req, res) {
   const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     },
-    condition,
-    (result) => {
+    condition, function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
       res.status(200).end();
 
-    }
-  );
+    });
+  });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 });
 
 // Export routes for server.js to use.
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
 /* Schema */
+DROP DATABASE IF EXISTS burger_db;
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE burger_db;
+
+USE burger_db;
 
 CREATE TABLE burgers
 (
