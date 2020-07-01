4154f8c24bcd55de6e0c95be30c0e6ff147ddc95
koyeary <katyeary@gmail.com>
2020-06-30 21:29:18 -0700

    Database generated

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+/* Sesame Seeds */
+
+INSERT INTO burgers (burger_name) VALUES ("New Bacon-ings");
+INSERT INTO burgers (burger_name) VALUES ("Mission A-Corn-Plished");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rest in Peas", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Foot Feta-ish", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Thank God it's Fried Egg", true);
+INSERT INTO burgers (burger_name) VALUES ("These Collards Don't Run");
+INSERT INTO burgers (burger_name) VALUES ("Camembert-ly Legal");
