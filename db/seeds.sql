4503362f62ff21e4dd6ed069bd45fc8123e7a265
koyeary <katyeary@gmail.com>
2020-07-02 21:54:12 -0700

    fixed typo

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,22 +2,17 @@
 
 USE burger_db;
 
-INSERT INTO burgers (burger_name) VALUES ("new bacon-ings");
-INSERT INTO burgers (burger_name) VALUES ("mission a-corn-plished");
-INSERT INTO burgers (burger_name, devoured) VALUES ("rest in peas", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("foot feta-ish", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("thank god it's fried egg", true);
-INSERT INTO burgers (burger_name) VALUES ("these collards don't run");
-INSERT INTO burgers (burger_name) VALUES ("camembert-ly legal");
-INSERT INTO burgers (burger_name, devoured) VALUES ("pepper don't preach", true);
-INSERT INTO burgers (burger_name) VALUES ("the roquefort files");
-INSERT INTO burgers (burger_name, devoured) VALUES ("i've created a meunster", true);
-INSERT INTO burgers (burger_name) VALUES ("good night and good leek");
-INSERT INTO burgers (burger_name) VALUES ("band on the bun");
-INSERT INTO burgers (burger_name) VALUES ("if looks could kale");
-INSERT INTO burgers (burger_name, devoured) VALUES ("dial m for mustard", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("12 hangry men", true);
-INSERT INTO burgers (burger_name) VALUES ("in the nick of thyme");
-INSERT INTO burgers (burger_name, devoured) VALUES ("ketchup in the rye", true);
-INSERT INTO burgers (burger_name) VALUES ("a high wind in jicama");
-INSERT INTO burgers (burger_name, devoured) VALUES ("i, cauliflower", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dial M for Mustard", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("12 Hangry Men", true);
+INSERT INTO burgers (burger_name) VALUES ("In the Nick of Thyme");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ketchup in the Rye", true);
+INSERT INTO burgers (burger_name) VALUES ("A High Wind in Jicama");
+INSERT INTO burgers (burger_name, devoured) VALUES ("I, Cauliflower", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Donut Let Me Down", true);
+INSERT INTO burgers (burger_name) VALUES ("The Bok-Choys are Back in Town");
+INSERT INTO burgers (burger_name) VALUES ("Rosemary F. Kill");
+INSERT INTO burgers (burger_name) VALUES ("The Pepperoncini in Your Stepperoncini");
+INSERT INTO burgers (burger_name) VALUES ("Dijon Tucker Must Die");
+INSERT INTO burgers (burger_name) VALUES ("To Kale a Mockingbird");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Girls Just Wanna Have Buns", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Don't be a Wienerschnizel", true);
