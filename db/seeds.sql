6a96c9507e3d3a1200a801fbc7435cebe94119fb
koyeary <katyeary@gmail.com>
2020-07-02 20:44:23 -0700

    commit style and seed adjustments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,23 @@
 /* Sesame Seeds */
 
-INSERT INTO burgers (burger_name) VALUES ("New Bacon-ings");
-INSERT INTO burgers (burger_name) VALUES ("Mission A-Corn-Plished");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Rest in Peas", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Foot Feta-ish", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Thank God it's Fried Egg", true);
-INSERT INTO burgers (burger_name) VALUES ("These Collards Don't Run");
-INSERT INTO burgers (burger_name) VALUES ("Camembert-ly Legal");
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ("new bacon-ings");
+INSERT INTO burgers (burger_name) VALUES ("mission a-corn-plished");
+INSERT INTO burgers (burger_name, devoured) VALUES ("rest in peas", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("foot feta-ish", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("thank god it's fried egg", true);
+INSERT INTO burgers (burger_name) VALUES ("these collards don't run");
+INSERT INTO burgers (burger_name) VALUES ("camembert-ly legal");
+INSERT INTO burgers (burger_name, devoured) VALUES ("pepper don't preach", true);
+INSERT INTO burgers (burger_name) VALUES ("the roquefort files");
+INSERT INTO burgers (burger_name, devoured) VALUES ("i've created a meunster", true);
+INSERT INTO burgers (burger_name) VALUES ("good night and good leek");
+INSERT INTO burgers (burger_name) VALUES ("band on the bun");
+INSERT INTO burgers (burger_name) VALUES ("if looks could kale");
+INSERT INTO burgers (burger_name, devoured) VALUES ("dial m for mustard", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("12 hangry men", true);
+INSERT INTO burgers (burger_name) VALUES ("in the nick of thyme");
+INSERT INTO burgers (burger_name, devoured) VALUES ("ketchup in the rye", true);
+INSERT INTO burgers (burger_name) VALUES ("a high wind in jicama");
+INSERT INTO burgers (burger_name, devoured) VALUES ("i, cauliflower", true);
