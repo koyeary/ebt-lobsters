28640535ac56d50584604b6e0767190c5b0e105e
koyeary <katyeary@gmail.com>
2020-07-07 00:31:31 -0700

    correct server connection glitch cleardb

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,6 @@ const connection = mysql.createConnection({
   password: "76f12c79",
   database: "heroku_d71186c4e54591c"
 });
-const del = connection._protocol._delegateError;
-connection._protocol._delegateError = function(err, sequence){
-  if (err.fatal) {
-    console.trace('fatal error: ' + err.message);
-  }
-  return del.call(this, err, sequence);
-};
-
 // Make connection.
 connection.connect(function(err) {
   if (err) {
